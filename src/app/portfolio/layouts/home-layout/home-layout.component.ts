24c714406ed3d5f43fe1c25be3e5e42144552147
Ana Isabel <anaisabelpatinoosorio1@gmail.com>
2025-07-22 15:10:34 -0500

    Design improvements

--- src/app/portfolio/layouts/home-layout/home-layout.component.ts
+++ src/app/portfolio/layouts/home-layout/home-layout.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { IntroComponent } from "../../components/intro/intro.component";
 import { ProjectsComponent } from "../../components/projects/projects.component";
 import { AboutComponent } from "../../components/about/about.component";
+import { FooterComponent } from "../../../shared/footer/footer.component";
 
 
 @Component({
   selector: 'app-store-front-layout',
-  imports: [RouterOutlet, NavbarComponent, IntroComponent, ProjectsComponent, AboutComponent],
+  imports: [RouterOutlet, NavbarComponent, IntroComponent, ProjectsComponent, AboutComponent, FooterComponent],
   templateUrl: './home-layout.component.html',
 })
 export class HomeLayoutComponent {}
